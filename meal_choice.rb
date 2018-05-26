bdd8b2d30f78a6aa8a5ebf99bc1e7efea10265b9
Tanner Lancaster <tanner@flatironschool.com>
2018-05-26 13:32:08 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,10 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice(choice)
-  if choice == "vegan"
-    return "vegan"
-  else
-    return "meat";
-  end
+def meal_choice(choice="meat")
+  return choice
 end
